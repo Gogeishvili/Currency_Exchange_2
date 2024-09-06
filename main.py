3e014e487f3705dd4c13f2886cde64e72489a92f
Gogeishvili <gio.gogeishvili@gmail.com>
2024-09-07 00:21:29 +0400

    instaled Pyqt5

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Init")
+import sys
+
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QDesktopWidget, QVBoxLayout, QPushButton, QHBoxLayout
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle("Layout Example")
+
+layout = QHBoxLayout()
+layout.addWidget(QLabel("this is label"))
+layout.addWidget(QPushButton("this is button"))
+
+window.setLayout(layout)
+
+window.show()
+sys.exit(app.exec_())
